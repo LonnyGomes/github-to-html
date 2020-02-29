bc526e957f867fdab4ffae81f7dcc3e1178f75fc
Lonny Gomes <code@lonnygomes.com>
2020-02-29 17:02:42 -0500

    chore: set default renderer to nunjucks

--- .eleventy.js
+++ .eleventy.js
@@ -9,7 +9,10 @@ module.exports = function (eleventyConfig) {
 
     return {
         dir: {
-            input: 'src'
-        }
+            input: 'src',
+        },
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk"
     }
 }
