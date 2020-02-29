2d51140d11e1d8f1001a13363ef7e1491a7a0924
Lonny Gomes <code@lonnygomes.com>
2020-02-29 14:56:06 -0500

    feat: add cache support for github issues
    based code off of this guide:
    https://www.11ty.dev/docs/quicktips/eliminate-js/

--- src/_data/issues.js
+++ src/_data/issues.js
@@ -1,18 +1,40 @@
 const { Octokit } = require("@octokit/rest");
+const flatcache = require('flat-cache');
+const path = require("path");
+
 const octokit = new Octokit();
 const owner = 'lonnygomes';
 const repo = 'vjs-video';
+
+const getCacheKey = () => {
+    const date = new Date();
+    return `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}`;
+}
+
 module.exports = async () => {
-    const { data } = await octokit.issues.listForRepo({
-        owner,
-        repo
-    });
+    const cache = flatcache.load("github-issues", path.resolve("./_datacache"));
+    const key = getCacheKey();
+    let results = cache.getKey(key);
 
-    return data.map((item) => {
-        item.tags = item.labels.map(label => label.name);
-        item.github_url = item.url;
-        Reflect.deleteProperty(item, 'url');
+    if (!results) {
 
-        return item;
-    });
+        const { data } = await octokit.issues.listForRepo({
+            owner,
+            repo
+        });
+
+        const newData = await data.map((item) => {
+            item.tags = item.labels.map(label => label.name);
+            item.github_url = item.url;
+            Reflect.deleteProperty(item, 'url');
+
+            return item;
+        });
+
+        cache.setKey(key, newData);
+        cache.save();
+        results = newData;
+    }
+
+    return results;
 }
