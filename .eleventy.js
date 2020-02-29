704449a6e740a567deb9f69dfdf1ebe2b71caf03
Lonny Gomes <code@lonnygomes.com>
2020-02-29 15:30:00 -0500

    feat: add basic index template that lists github issues

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 
 
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/css");
+
     eleventyConfig.addShortcode("ghLabels", (labels) => {
         return labels.map(label => `<span style="background-color:#${label.color}" class="label">${label.name}</span>`);
     });
